7d22bab68522ffa2db515d4a7a3fa6f4dc79d9ab
Mohiitdeshmukh <mohit17deshmukh@gmail.com>
2023-05-06 23:04:04 +0530

    Phase 1

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -30,13 +30,13 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      height: 300,
-                      child: Image.asset('assets/images/register.png'),
-                    ),
-                  ),
+                  // Hero(
+                  //   tag: 'logo',
+                  //   child: Container(
+                  //     height: 300,
+                  //     child: Image.asset('assets/images/register.png'),
+                  //   ),
+                  // ),
               // SizedBox(
               //   height: 10.0,
               // ),
